5fbe49179f0b7cb30865080845e292b30638ff78
flycool <flycoolcool@163.com>
2023-01-04 11:37:48 +0800

    scroll to current time index

--- app/src/main/java/com/example/maxweather/presentation/ui/theme/WeatherForecast.kt
+++ app/src/main/java/com/example/maxweather/presentation/ui/theme/WeatherForecast.kt
@@ -5,13 +5,17 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.maxweather.presentation.WeatherState
+import kotlinx.coroutines.launch
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
@@ -19,6 +23,8 @@ fun WeatherForecast(
     modifier: Modifier = Modifier,
     state: WeatherState,
 ) {
+    val currentData = state.weatherInfo?.currentWeatherData
+
     state.weatherInfo?.weatherDataPerDay?.get(0)?.let { data ->
         println("data $data")
         Column(
@@ -32,7 +38,13 @@ fun WeatherForecast(
                 color = Color.White
             )
             Spacer(modifier = Modifier.height(16.dp))
-            LazyRow() {
+
+            val lazyListState = rememberLazyListState()
+            val scope = rememberCoroutineScope()
+
+            LazyRow(
+                state = lazyListState
+            ) {
                 items((data)) { weatherData ->
                     HourlyWeatherDisplay(
                         weatherData = weatherData,
@@ -40,6 +52,11 @@ fun WeatherForecast(
                             .height(100.dp)
                             .padding(horizontal = 16.dp)
                     )
+                }
+                scope.launch {
+                    val scrollToIndex =
+                        if ((data.indexOf(currentData) - 1) < 0) 0 else data.indexOf(currentData) - 1
+                    lazyListState.animateScrollToItem(scrollToIndex)
                 }
             }
         }
